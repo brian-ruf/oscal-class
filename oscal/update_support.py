d9935cb2966194d384a4b2cd5dff5a8096ae828a
Brian Ruf <brianruf@gmail.com>
2026-03-08 13:24:49 -0400

    added  script to refresh and package support database

--- oscal/update_support.py
+++ oscal/update_support.py
@@ -1,25 +1,24 @@
 from oscal.oscal_support_class import setup_support
 import os
-import zipfile
+from loguru import logger
+from ruf_common.lfs import zip_file
 
 SUPPORT_DB_PATH = os.path.join("..", "support", "oscal_support.db")
 SUPPORT_ZIP_PATH = os.path.join("data", "oscal_support.zip")
 
-print ("Database absolute path: " + os.path.abspath(SUPPORT_DB_PATH))
+logger.info("Database absolute path: " + os.path.abspath(SUPPORT_DB_PATH))
 supportObj = setup_support(SUPPORT_DB_PATH)
 
-if True: # supportObj.update("all"):
-    print("Support assets updated successfully.")
-    # Clear ./data/oscal_support.zip to ensure it will be regenerated with the updated support assets
-    if os.path.exists(SUPPORT_ZIP_PATH):
-        os.remove(SUPPORT_ZIP_PATH)
-        print("Cleared existing oscal_support.zip to allow regeneration with updated assets.")
+
+if supportObj.update("all"):
+    logger.info("Support assets updated successfully.")
 
     # Zip the updated support database for distribution
-    with zipfile.ZipFile(SUPPORT_ZIP_PATH, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        zipf.write(SUPPORT_DB_PATH, arcname=os.path.basename(SUPPORT_DB_PATH))
-    print(f"Updated support database compressed and saved to {SUPPORT_ZIP_PATH}.")
+    if zip_file(SUPPORT_DB_PATH, SUPPORT_ZIP_PATH, overwrite=True):
+        logger.info(f"Updated support database compressed and saved to {SUPPORT_ZIP_PATH}.")
+    else:
+        logger.error("Failed to compress the updated support database.")
     
 
 else:    
-    print("Failed to update support assets.")
+    logger.error("Failed to update support assets.")
